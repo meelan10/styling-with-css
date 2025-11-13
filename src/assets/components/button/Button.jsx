d7d379c6f118bcadfa34490e303b4f81602f8a91
meelan10 <milanwagle12@gmail.com>
2025-11-13 23:03:23 +0545

    Updated CSS styling or component

---
+++ src/assets/components/button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>
+      <div>
+        This is button
+      </div>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/assets/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from'./Header.module.css';
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h3>Milan wagle </h3>
+      <button className='btn'>login</button>
+      
+    </div>
+  )
+}
+
+export default Header
